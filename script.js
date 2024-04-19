762463e1eca9a8858c199740b23100c6477e70b5
Revathi1206 <126352489+Revathi1206@users.noreply.github.com>
2024-04-19 20:30:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const firstName = document.getElementById("valid1");
 const lastName = document.getElementById("valid2");
 const address = document.getElementById("valid4");
 const phone = document.getElementById("valid3");
+const emailId = document.getElementById("valid6");
+const Date = document.getElementById("valid5");
+
 const fname = document.querySelector(".fname");
 const lname = document.querySelector(".lname");
 const phn = document.querySelector(".phn");
 const add = document.querySelector(".address");
+const email = document.querySelector(".email");
+const date = document.querySelector(".date");
+
 const popup = document.querySelector(".popup-msg")
 const clear = document.querySelector(".cl-btn")
 const hiddenBox = document.querySelector(".details-hidden");
+
+
 
 
 const forms = document.querySelectorAll('.needs-validation')
@@ -29,11 +37,17 @@ Array.from(forms).forEach(form => {
 
 submit.addEventListener("click", function (e) {
     console.log(phone.value.length);
-    if (firstName.value && lastName.value && address.value && phone.value) {
+    if (firstName.value && lastName.value && address.value && phone.value && emailId.value && Date.value) {
         e.preventDefault();
         if ((/[a-zA-Z]/.test(phone.value) == true) || (phone.value.length != 10)) {
             popup.style.display = "block";
             phone.value = '';
+
+        }
+        else if (emailId.value.includes("@") == false && emailId.value.includes(".") == false) {
+            popup.style.display = "block";
+            popup.textContent = "Invaid Email id"
+            emailId.value = '';
 
         }
         else if (firstName.value.includes(" ") == false && lastName.value.includes(" ") == false) {
@@ -42,6 +56,9 @@ submit.addEventListener("click", function (e) {
             lname.textContent = lastName.value;
             phn.textContent = phone.value;
             add.textContent = address.value;
+            email.textContent = emailId.value;
+            date.textContent = Date.value;
+
             // firstName.value = lastName.value = address.value = phone.value = '';
             hiddenBox.style.display = "block";
             scrolldown.style.opacity = 1;
@@ -58,7 +75,7 @@ submit.addEventListener("click", function (e) {
 })
 
 clear.addEventListener("click", function () {
-    firstName.value = lastName.value = address.value = phone.value = '';
+    firstName.value = lastName.value = address.value = phone.value = email.value = date = '';
 
 })
 
